9fc74555cc86fc74598b66c47d31314bfeb1108c
Richd0tcom <tuberich0@gmail.com>
2022-12-22 23:17:00 +0100

    install dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminModule } from './admin/admin.module';
+import { SupplierModule } from './supplier/supplier.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AdminModule, SupplierModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
